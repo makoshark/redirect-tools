46d70e0e1f81192e73ae976cdf16138a428f88aa
aaronshaw <aaron.d.shaw@gmail.com>
2014-07-02 13:25:21 -0500

    debugging the command to write output to tsv. inserted a comma and changed it to write.table

--- 02-generate_spells.R
+++ 02-generate_spells.R
@@ -10,10 +10,8 @@ setwd(cur.dir)
 setwd(spells.data.dir)
 save(redirect.spells, file="redirect_spells.RData")
 
-write.csv(redirect.spells, file="redirect_spells.tsv", sep="\t"
+write.table(redirect.spells, file="redirect_spells.tsv", sep="\t",
                  fileEncoding="UTF-8", row.names=FALSE)
 
 library(foreign)
 write.dta(redirect.spells, file="redirect_spells.dta")
-
-
